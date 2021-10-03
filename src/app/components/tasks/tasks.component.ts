b8532a022429aabfc066d6285493773e2be5ec12
LeeAnn Grapes <leeann.grapes@gmail.com>
2021-10-02 20:21:32 -0600

    Subscribed to task service observable

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
 import { Task } from '../../Task';
-import { TASKS } from '../../mock-tasks';
 
 @Component({
   selector: 'app-tasks',
@@ -10,13 +10,14 @@ import { TASKS } from '../../mock-tasks';
 export class TasksComponent implements OnInit {
 
   //assign as property of the component
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
+  //will eventually use an Observable here bc dealing with asynchronous data from server fetch
+  //.subscribe is like a promise
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
   }
-
- 
 
 }
