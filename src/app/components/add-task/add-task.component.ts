53dafb2da376f1ebe1e054cf93a077dc9554ba45
LeeAnn Grapes <leeann.grapes@gmail.com>
2021-10-02 22:16:00 -0600

    Created onSubmit method with basic validation, an event emitter and form clearing

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-add-task',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTaskComponent implements OnInit {
 
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter;
+
+  text!: string;
+  day!: string;
+  reminder: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    //basic validation
+    if(!this.text || !this.day) {
+      alert('Please add a task');
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    }
+
+    this.onAddTask.emit(newTask);
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+
   }
 
 }
